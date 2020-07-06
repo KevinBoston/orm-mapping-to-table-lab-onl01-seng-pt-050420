5e8120c3821f6e601b870c61d13e0dcaf104ab2a
Kevin Boston <kbos22@gmail.com>
2020-07-06 14:58:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
     @grade = grade
   end
   def self.create_table
-    sql = <<- SQL 
+    sql = <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
